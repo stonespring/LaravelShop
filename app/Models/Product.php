6ea5ff17620af0ad192869e2c545f881d906c517
stone <123376536@qq.com>
2019-04-13 17:58:56 +0800

    前台 用户界面 -商品列表

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -19,5 +21,18 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class); //1对多关联
+    }
+
+    /**
+     * 图片URL获取
+     * // 如果 image 字段本身就已经是完整的 url 就直接返回
+     * 这里 \Storage::disk('public') 的参数 public 需要和我们在 config/admin.php 里面的 upload.disk 配置一致。
+     */
+    public function getImageUrlAttribute()
+    {
+        if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])){
+            return $this->attributes['image'];
+        }
+        return Storage::disk('public')->url($this->attributes['image']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::get('/', 'PagesController@root')->name('root')->middleware('verified');  // 在之前的路由后面配上中间件
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+//Route::get('/', 'PagesController@root')->name('root')->middleware('verified');  // 在之前的路由后面配上中间件
 Auth::routes(['verify' => true]); // 在之前的路由里加上一个 verify 参数
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
