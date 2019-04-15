f2fc578c148b3735f52fc07ad252959b05aa027b
stone <123376536@qq.com>
2019-04-15 17:02:09 +0800

    商品加入购物车

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,13 @@ class User extends Authenticatable implements MustVerifyEmail
             ->withTimestamps()
             ->orderBy('user_favorite_products.created_at', 'desc');
     }
+
+    /**
+     * 购物车
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     //收藏列表
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
+    Route::post('cart', 'CartController@add')->name('cart.add');
+
 });
 //Route::get('/home', 'HomeController@index')->name('home');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
